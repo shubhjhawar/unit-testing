91307f804c42b1cfc0598780ffa4195bde783f79
Shubh <shubhjhawar78@gmail.com>
2024-01-31 00:41:48 -0500

    learnt unit testing

---
+++ jest_tutorial/sum.test.js
@@ -0,0 +1,13 @@
+// const sum = require("./sum")
+
+// describe("example tests", ()=>{
+//     it("should add 1+2=3", () => {
+//         const result = sum(1,2)
+//         expect(result).toBe(3)
+//     })
+
+//     it("object assessment", () => {
+//         const obj = {}
+//         expect(obj).toEqual({})
+//     })
+// })
